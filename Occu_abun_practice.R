4e3433e49c3e135a303b43252b97e8e5a19fe595
reedetile <rscott1294@gmail.com>
2024-11-25 16:04:33 -0500

    Setup simulation of abundance data. Created what I think is a functional epi model

--- Occu_abun_practice.R
+++ Occu_abun_practice.R
@@ -3,64 +3,122 @@
 #  22 Nov 2024
 #RCS
 
+# Load packages---------------------------------
+library(vegan)
 # Parameters-------------------------------------
 #setting up meta-community parameters
+set.seed(1234)
 num_patches <- 5 #number of patches in metacommunity
 num_spp <- 6 #number of POSSIBLE spp in metacommunity
 
-spp_occ <- data.frame(matrix(NA, nrow = num_patches, ncol = num_spp))
+meta_comm1 <- data.frame(matrix(NA, nrow = num_patches, ncol = num_spp))
 S <- c(0.95,0.9,0.75,0.50,0.20,0.1) #an array of probability values for the occurence of each spp
-
-#determine occurence of spp 1
-for(i in 1:nrow(spp_occ)){
-  for(j in 1:ncol(spp_occ)){
-    spp_occ[i,1] <- rbinom(n = 1, size = 1, prob = S[1])
-  }
-}
-
-#determine occurrence of spp 2
-for(i in 1:nrow(spp_occ)){
-  for(j in 1:ncol(spp_occ)){
-    spp_occ[i,2] <- ifelse(spp_occ[i,1] == 1, rbinom(n = 1, size = 1, prob = S[2]),0)
-  }
-}
-
-#determine occurrence of spp 3
-for(i in 1:nrow(spp_occ)){
-  for(j in 1:ncol(spp_occ)){
-    spp_occ[i,3] <- ifelse(spp_occ[i,2] == 1, rbinom(n = 1, size = 1, prob = S[3]),0)
-  }
-}
-
-#determin occurrence of spp 4
-for(i in 1:nrow(spp_occ)){
-  for(j in 1:ncol(spp_occ)){
-    spp_occ[i,4] <- ifelse(spp_occ[i,3] == 1, rbinom(n = 1, size = 1, prob = S[4]),0)
-  }
-}
-
-#determine occupancy of spp 5
-for(i in 1:nrow(spp_occ)){
-  for(j in 1:ncol(spp_occ)){
-    spp_occ[i,5] <- ifelse(spp_occ[i,4] == 1, rbinom(n = 1, size = 1, prob = S[5]),0)
-  }
-}
-
-#determine occupancy of spp 6
-for(i in 1:nrow(spp_occ)){
-  for(j in 1:ncol(spp_occ)){
-    spp_occ[i,6] <- ifelse(spp_occ[i,5] == 1, rbinom(n = 1, size = 1, prob = S[6]),0)
-  }
-}
-
-M <- 3
-Y0 <- 10
-z <- 0.1 #constant.
-P <- 1:num_spp #a vector. Preston's rankThe rank for each spp, which should correspond to the assigned abundance
-K <- c(80,70,60,50,40,30,20,10) #this is just an example, but k is the abundance at each rank (i think?)
+K <- c(60,50,40,30,20,10) #this is just an example, but k is the abundance at each rank (i think?)
 #this could later become some sort of rnorm(). EX rnorm(n = 1, mean = 10, sd = 1). This provides a starting abdunance for each spp if present
-rank_abun <- Y0*exp(-(z*P-M)^2)
-plot(rank_abun)
 
-abun <- data.frame(K, rank_abun)
-plot(abun$rank_abun, abun$K)  
+### Creating a meta-community###
+#For right now I have it set where the presence / absence of a spp is a binomial variable with some
+# Probability S[i]
+#IF the species is present, then it is present at an abundance of K[i]. Right now this abundance is fixed.
+# One idea for future development is to make abundance a distribution (could even just turn it into a poisson distribution?)
+#Other idea/issue: if abundance of a "prior" spp is 0 than abundance of all following spp is 0. This is based on the idea that ecological communities are nested within one-another. However, instead of guaranteeing that abundance of next spp = 0, I wonder if it makes more sense to just make the probability of occ / abundance lower (but not set to zero.
+
+# I think there's a lot that could be done here. A lot could be adapted. But this set of for loops is a decent start
+
+
+# #determine occurence of spp 1
+# for(i in 1:nrow(meta_comm1)){
+#   for(j in 1:ncol(meta_comm1)){
+#     meta_comm1[i,1] <- rbinom(n = 1, size = 1, prob = S[1])
+#     meta_comm1[i,1] <- ifelse(meta_comm1[i,1] == 1, K[1],0)
+#   }
+# }
+# 
+# #determine occurrence of spp 2
+# for(i in 1:nrow(meta_comm1)){
+#   for(j in 1:ncol(meta_comm1)){
+#     meta_comm1[i,2] <- ifelse(meta_comm1[i,1] > 0, rbinom(n = 1, size = 1, prob = S[2]),0)
+#     meta_comm1[i,2] <- ifelse(meta_comm1[i,2] == 1, K[2],0)
+#   }
+# }
+# 
+# #determine occurrence of spp 3
+# for(i in 1:nrow(meta_comm1)){
+#   for(j in 1:ncol(meta_comm1)){
+#     meta_comm1[i,3] <- ifelse(meta_comm1[i,2] > 0, rbinom(n = 1, size = 1, prob = S[3]),0)
+#     meta_comm1[i,3] <- ifelse(meta_comm1[i,3] == 1, K[3],0)
+#   }
+# }
+# 
+# #determin occurrence of spp 4
+# for(i in 1:nrow(meta_comm1)){
+#   for(j in 1:ncol(meta_comm1)){
+#     meta_comm1[i,4] <- ifelse(meta_comm1[i,3] > 0, rbinom(n = 1, size = 1, prob = S[4]),0)
+#     meta_comm1[i,4] <- ifelse(meta_comm1[i,4] == 1, K[4],0)
+#   }
+# }
+# 
+# #determine occupancy of spp 5
+# for(i in 1:nrow(meta_comm1)){
+#   for(j in 1:ncol(meta_comm1)){
+#     meta_comm1[i,5] <- ifelse(meta_comm1[i,4] > 0, rbinom(n = 1, size = 1, prob = S[5]),0)
+#     meta_comm1[i,5] <- ifelse(meta_comm1[i,5] == 1, K[5],0)
+#   }
+# }
+# 
+# #determine occupancy of spp 6
+# for(i in 1:nrow(meta_comm1)){
+#   for(j in 1:ncol(meta_comm1)){
+#     meta_comm1[i,6] <- ifelse(meta_comm1[i,5] > 0, rbinom(n = 1, size = 1, prob = S[6]),0)
+#     meta_comm1[i,6] <- ifelse(meta_comm1[i,6] == 1, K[6],0)
+#   }
+# }
+
+# I THINK this is a better way to determine abundance
+for(i in 1:nrow(meta_comm1)){
+  for(j in 1:ncol(meta_comm1)){
+    #determine occurrence and abundance of spp 1
+    meta_comm1[i,1] <- rbinom(n = 1, size = 1, prob = S[1])
+    meta_comm1[i,1] <- ifelse(meta_comm1[i,1] == 1, K[1],0)
+    
+    #determine occurrence and abundance of spp2
+    meta_comm1[i,2] <- ifelse(meta_comm1[i,1] > 0, rbinom(n = 1, size = 1, prob = S[2]),0)
+    meta_comm1[i,2] <- ifelse(meta_comm1[i,2] == 1, K[2],0)
+    
+    #determine occurrence and abundance of spp 3
+    meta_comm1[i,3] <- ifelse(meta_comm1[i,2] > 0, rbinom(n = 1, size = 1, prob = S[3]),0)
+    meta_comm1[i,3] <- ifelse(meta_comm1[i,3] == 1, K[3],0)
+    
+    #determine occurrence and abundance of spp 4
+    meta_comm1[i,4] <- ifelse(meta_comm1[i,3] > 0, rbinom(n = 1, size = 1, prob = S[4]),0)
+    meta_comm1[i,4] <- ifelse(meta_comm1[i,4] == 1, K[4],0)
+    
+    #determine occurrence and abundance of spp 5
+    meta_comm1[i,5] <- ifelse(meta_comm1[i,4] > 0, rbinom(n = 1, size = 1, prob = S[5]),0)
+    meta_comm1[i,5] <- ifelse(meta_comm1[i,5] == 1, K[5],0)
+    
+    #determine occurrence and abundance of spp 6
+    meta_comm1[i,6] <- ifelse(meta_comm1[i,5] > 0, rbinom(n = 1, size = 1, prob = S[6]),0)
+    meta_comm1[i,6] <- ifelse(meta_comm1[i,6] == 1, K[6],0)
+  }
+}
+
+meta_comm1$Patch <- c('Patch1','Patch2','Patch3','Patch4','Patch5')
+colnames(meta_comm1) <- c('Spp1','Spp2','Spp3','spp4','spp5','Spp6','PatchID')
+beta_diversity <- betadiver(meta_comm1[,1:6], method = 'w')
+plot(beta_diversity)
+
+
+### Below is use of Preston's law. Not sure we'll actually be doing this, so I have commented it out for
+##now
+# M <- 3
+# Y0 <- 10
+# z <- 0.1 #constant.
+# P <- 1:num_spp #a vector. Preston's rankThe rank for each spp, which should correspond to the assigned abundance
+# K <- c(80,70,60,50,40,30,20,10) #this is just an example, but k is the abundance at each rank (i think?)
+# #this could later become some sort of rnorm(). EX rnorm(n = 1, mean = 10, sd = 1). This provides a starting abdunance for each spp if present
+# rank_abun <- Y0*exp(-(z*P-M)^2)
+# plot(rank_abun)
+# 
+# abun <- data.frame(K, rank_abun)
+# plot(abun$rank_abun, abun$K)  
--- epimodel.R
+++ epimodel.R
@@ -5,37 +5,60 @@
 
 #Initialize -----------------------------------------
 library(vegan)
-
+source('Occu_abun_practice.R')
 # Load functions--------------------------------------
 
 
 # Parameters-------------------------------------
-p <- #number of communities
-sp <- #number of species POSSIBLE in metacommunity
+#starting values
+S <- ceiling(meta_comm1[,1:6]*c(0.8,0.85,0.9,0.93,0.95,.99)) #starting value of susceptibles
+I <- meta_comm1[,1:6] - S #starting value of infecteds
+N <- S+I
+Z <- 1000 #starting amount of zoospores
 
-#how to determine rank abundance of species
-M <- 3
-Y0 <- 10
-q <- 0.1 #constant. Used q instead of z b/c z represents zoospores in this work
-P <- 1:8 #a vector. The rank for each spp, which should correspond to the assigned abundance
-rank_abun <- Y0*exp(-(q*P-M)^2)
-plot(rank_abun)
+#species characteristics
+Species <- c("Spp1","Spp2","Spp3","Spp4","spp5","spp6")
+b <- c(0.6,0.5,0.4,0.3,0.2,0.1) #host birth rate
+d <- c(0.06,0.05,0.04,0.03,0.02,0.01) #host death rate
+beta <- c(0.00013,0.00012,0.00011,0.00010,0.00009,0.00008) #transmission per species
+v <- c(0.4,0.5,0.6,0.7,0.8,0.9) #recovery rate
+phi <- c(0.09,0.08,0.07,0.06,0.05,0.04) #dispersal rate
+lambda <- c(3.0,2.5,2.0,1.5,1.0,0.5) #log(bd) load. losely based off figure 2 from wilber 2020 
+species_chara <- data.frame(Species = Species,
+                            birth = b, 
+                            death = d, 
+                            trans = beta, 
+                            recovery = v, 
+                            dispersal = phi, 
+                            shedding = lambda)
 
-for(i in length(P)){
-  
-
-N <- rnorm(
-  #A matrix of p * sp. Total number of individuals in patch p of species sp. Should be S + I
-
-S <- #A matrix of p * sp. number of susceptible individuals for each species at each patch
-I <- #A matrix of p * sp. number of infectious individuals for each species at each patch
-
-
-
+# meta-community characteristics
+gamma <- 0.001 #zoospore decay rate
+c <- matrix(data = rnorm(n = num_patches^2, mean = 0.5, sd = 0.1),
+            nrow = num_patches, 
+            ncol = num_patches)
+#Connectivity of patches
+A <- rnorm(n = num_patches, mean = 0.5, sd = 0.1) #area ratios
+time <- 90 #how many "days" do I want in the season
 
 # Program Body------------------------------------------
-delta_s <-b*N - d*S - beta*S*Z+v*I+phi*sum(-c*S+c*S*A[j]/A[p])
-delta_I <- beta*S*Z-(v+d)*I+phi*sum(-c*I + c*I*A[j]/A[p])
-delta_Z <- sum(lambda*I - gamma*Z)
+pop_list <- vector("list", length = time)
+for (t in 1:time) {
+  delta_s <- b*N - d*S - beta*S*Z + v*I + phi*sum(-c*S + c*S) #for now have excluded area of patches
+  #may want to add that back in though
+  delta_I <- beta*S*Z-(v+d)*I+phi*sum(-c*I + c*I)#for now have excluded area of patches
+  #may want to add that back in though
+  delta_Z <- sum(lambda*I - gamma*Z)
+  S <- S+delta_s
+  I <- I+delta_I
+  z <- Z+delta_Z
+  N <- S + I
+  t = t
+  pop <- list(Susceptible = S, Infectious = I, Zoospores = Z, Total = N, Time = t)
+  pop_list[[t]] <- pop
+}
+  
+pop_data <- do.call(rbind, pop_list)
+pop_data  
 
 
